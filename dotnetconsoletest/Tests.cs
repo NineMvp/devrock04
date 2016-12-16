eef14bf9722ab13cf65d8faadd251b0a7a02c39b
Areepong, Chalermpon (Agoda) <chalermpon.areepong@agoda.com>
2016-12-17 05:08:01 +0700

    initial sources

---
+++ dotnetconsoletest/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using ClassLibrary;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestCountString() 
+        {
+            var testWord = "DEVROCK#04";
+            
+            Assert.True(StringLib.Count(testWord) > 10);
+        }
+    }
+}
---
+++ dotnetlib/Library.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace ClassLibrary
+{
+    public class StringLib
+    {
+        public static int Count(String s)
+        {
+           return s.Count();
+        }
+    }
+}
